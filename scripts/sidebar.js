db32ecf77da4e6f71d6ead960485f77a9f9099d1
Daniel Zhao <65431300+djzh01@users.noreply.github.com>
2022-04-06 00:43:58 -0400

    fix iframe usage

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,9 +1,22 @@
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+
 var btns = document.getElementById("region-buttons").children;
 var sidebar = document.getElementById("sidebar");
-
+console.log(btns);
 Array.from(btns).forEach(btn => {
+    console.log(btn);
     btn.addEventListener('click', function (){
         if(sidebar.classList.contains('active')) sidebar.classList.remove('active');
         else sidebar.classList.add('active');
     });
 });
+btns.onclick = function (e) {
+    console.log('click');
+}
