740d443f81b17b28cb1aaeeee25ec20685c52dd0
Daniel Zhao <65431300+djzh01@users.noreply.github.com>
2022-04-06 00:40:13 -0400

    fixed buttons itneracting with sidebar

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,13 +1,9 @@
-$(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
+var btns = document.getElementById("region-buttons").children;
+var sidebar = document.getElementById("sidebar");
+
+Array.from(btns).forEach(btn => {
+    btn.addEventListener('click', function (){
+        if(sidebar.classList.contains('active')) sidebar.classList.remove('active');
+        else sidebar.classList.add('active');
     });
 });
-
-function on() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off() {
-    document.getElementById("overlay").style.display = "none";
-  }
