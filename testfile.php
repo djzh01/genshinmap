20c4258e0fcabf2c013844040d6312582a3db00e
glc6qrx <georgecao2000@gmail.com>
2022-04-27 00:57:51 -0400

    characters working

--- testfile.php
+++ testfile.php
@@ -1,13 +1,13 @@
 <?php
-include("Database.php");
+include("classes/Database.php");
 session_start();
 
 $db = new Database();
 #where not exists (select * from genshin_saved
 #where name = $_POST[name] and id = $_SESSION[id]
 
- $insert = $db->query("insert into genshin_saved (user_id, name, category) values (?, ?, ?)", 
- "iss", $_SESSION["id"], $_POST["name"], $_POST["category"]);
+ $insert = $db->query("insert into genshin_saved (user_id, entity_id) values (?, ?)", 
+ "ii", $_SESSION["id"], $_POST["entity_id"]);
 if (!$insert) {
 $error_msg = "Error adding transaction";
 } else {
